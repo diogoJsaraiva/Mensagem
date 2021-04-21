2cd8472410b17264bc98bd89542591efba39d2d8
Diogo Saraiva <diogo.saraiva12@hotmail.com>
2021-04-21 10:56:29 +0100

    Colocar a mensagem visivel no campo de mensagem

--- app/src/main/java/com/example/mensagem/MostraInformacaoActivity.kt
+++ app/src/main/java/com/example/mensagem/MostraInformacaoActivity.kt
@@ -2,10 +2,18 @@ package com.example.mensagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MostraInformacaoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostra_informacao)
+
+        val textViewMensagem = findViewById<TextView>(R.id.textViewMensagem)
+        val mensagem = intent.getStringExtra("MENSAGEM")
+
+
+        textViewMensagem.setText(mensagem)
+
     }
 }
