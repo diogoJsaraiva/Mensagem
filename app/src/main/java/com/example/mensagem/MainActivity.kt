61ec2814ae0f0e318e5bbada07547fb47847101a
Diogo Saraiva <diogo.saraiva12@hotmail.com>
2021-04-28 09:20:57 +0100

    Obter a data e passa para a outra atividade

--- app/src/main/java/com/example/mensagem/MainActivity.kt
+++ app/src/main/java/com/example/mensagem/MainActivity.kt
@@ -6,11 +6,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import java.time.LocalDateTime
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     companion object{
         val INFO_EXTRA_MENSAGEM = "MENSAGEM"
-        val INFO_DATA = "DATA"
+        const val INFO_DATA = "DATA"
+
     }
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -25,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         val mensagem = editTextMensagem.text.toString()
 
 
-        val data = LocalDateTime.
+
 
 
 
@@ -34,10 +36,16 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        val intent = Intent(this,MostraInformacaoActivity::class.java)
+        
+        val data = Date()
 
-        intent.putExtra(INFO_EXTRA_MENSAGEM , mensagem)
-        intent.putExtra(INFO_DATA,data)
+        val intent = Intent(this,MostraInformacaoActivity::class.java).apply {
+            putExtra(INFO_EXTRA_MENSAGEM , mensagem)
+            putExtra(INFO_DATA , data)
+        }
+
+        //intent.putExtra(INFO_EXTRA_MENSAGEM , mensagem)
+        //intent.putExtra(INFO_DATA , data)
 
 
 
